7873da33b726a22f1f685356b49a0514357a5e29
Omadoye Jedidiah <jedidiah.omadoye@andela.com>
2017-06-26 17:47:32 +0100

    deploy to heroku

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -12,7 +12,7 @@ app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='admin'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
